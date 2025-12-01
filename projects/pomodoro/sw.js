fddc2863c990163e7239004cdaff9bef910b27ed
Seokjun Ryu <rsj0520@naver.com>
2025-12-01 23:55:17 +0900

    update qwa

---
+++ projects/pomodoro/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'pomodoro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// 설치 시 캐시에 파일 저장
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('캐시 열림');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 네트워크 요청 가로채서 캐시된 버전 제공
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 캐시에 있으면 캐시된 버전 반환
+        if (response) {
+          return response;
+        }
+        // 없으면 네트워크에서 가져오기
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 새 버전 설치 시 이전 캐시 삭제
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
